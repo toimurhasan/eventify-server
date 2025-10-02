afb559233ca104b46df0674612633a50502b6eee
toimurhasan <2imur.hasan@gmail.com>
2025-10-02 23:10:20 +0600

    installed express server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = process.env.port || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+app.listen(port, () => {
+  console.log(`running on port ${port}`);
+});
