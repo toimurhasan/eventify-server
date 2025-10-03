a4e72912d11b6fd78b418647553e3c87898b0707
toimurhasan <2imur.hasan@gmail.com>
2025-10-03 10:48:36 +0600

    added demo post req

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.port || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://dbUser:randomPassword@cluster0.2rn0ff6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // connect to db and collection
+    const database = client.db("eventify_db");
+    const events = database.collection("events");
+
+    // send req
+    app.post("/createGroup", async (req, res) => {
+      console.log(req.body);
+      const result = await events.insertOne(req.body);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello world");
